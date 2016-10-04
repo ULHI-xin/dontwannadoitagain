31b8941bfce8cc96a63f5fa71702bebe301eeb94
ULHI-Xin <xin@upwlabs.com>
2016-10-04 22:53:23 +0800

    xynlqy downloader

--- eh-scrawler/base.py
+++ eh-scrawler/base.py
@@ -1,4 +1,5 @@
 
+import subprocess as sub
 
 from httplib2 import Http
 
@@ -16,3 +17,36 @@ def html_from_url(url, cookie):
                 return ctnt, resp['set-cookie']
         except:
             continue
+
+
+def mkdir_with_autocreate(dst):
+    p = sub.Popen(['mkdir', '-p', dst], stdout=sub.PIPE,
+                  stderr=sub.PIPE)
+    output, errors = p.communicate()
+
+
+def download_img(url, dst):
+    print "download ", url
+    ctnt = None
+    for _ in xrange(5):
+        print "_try ", _
+        try:
+            resp, ctnt = hc.request(url)
+            if '200' == resp.get('status'):
+                break
+        except Exception as e:
+            print "Download failed: ", e
+            continue
+
+    # print ctnt
+    if ctnt is None:
+        print "Failed img:", url
+        return
+    with open(dst, 'wb') as f:
+        f.write(ctnt)
+
+
+def cp(old, new):
+    p = sub.Popen(['cp', old, new], stdout=sub.PIPE,
+                  stderr=sub.PIPE)
+    output, errors = p.communicate()
