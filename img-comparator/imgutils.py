601207f2ab256937c18c11b3a49117b7cee602d0
ULHI-Xin <xin@upwlabs.com>
2016-11-05 15:37:02 +0800

    refine gif compressor

--- img-comparator/imgutils.py
+++ img-comparator/imgutils.py
@@ -2,6 +2,7 @@ import re
 import subprocess as sub
 from contextlib import closing
 from PIL import Image
+import os
 
 import requests
 from httplib2 import Http
@@ -26,29 +27,54 @@ def img_size_from_url(url):
             return None
 
 
-def shunk_gif(infile, outfile, **kwargs):
+def shunk_gif(infile, outfile, target_size, **kwargs):
     print "shunk_gif:", infile
-    DEFAULTS = {
-        'colors': '64',
-        'scale': '0.4',
-    }
-    args = {k: v for k, v in DEFAULTS.iteritems()}
-    for k in DEFAULTS:
-        if k in kwargs:
-            args[k] = kwargs[k]
-    p = sub.Popen(['gifsicle', ] + \
+    if kwargs:
+        print "  shrunk with args", kwargs
+        args = {k: v for k, v in kwargs.iteritems()}
+        p = sub.Popen(['gifsicle', ] + \
                   sum([["--" + k, v] for k, v in args.iteritems()], []) +\
                   [infile, '-o', outfile],
                   stdout=sub.PIPE, stderr=sub.PIPE)
-    output, errors = p.communicate()
-    if errors:
-        print errors
+        output, errors = p.communicate()
+        if errors:
+            print errors
+    else:
+        print "  auto shrunk"
+        target_size = 2000000 if target_size is None else target_size
+        ARGS = [
+            {'colors': '256', },
+            {'colors': '256', 'scale': '0.75'},
+            {'colors': '256', 'scale': '0.6'},
+            {'colors': '256', 'scale': '0.4'},
+            {'colors': '128', },
+            {'colors': '128', 'scale': '0.75'},
+            {'colors': '128', 'scale': '0.6'},
+            {'colors': '128', 'scale': '0.4'},
+            {'colors': '64', },
+            {'colors': '64', 'scale': '0.75'},
+            {'colors': '64', 'scale': '0.6'},
+            {'colors': '64', 'scale': '0.4'},
+            {'colors': '32', 'scale': '0.3'},
+        ]
+        for args in ARGS:
+            p = sub.Popen(['gifsicle', ] + \
+                          sum([["--" + k, v] for k, v in args.iteritems()], []) +\
+                          [infile, '-o', outfile],
+                          stdout=sub.PIPE, stderr=sub.PIPE)
+            output, errors = p.communicate()
+            if errors:
+                print errors
+            _size = os.path.getsize(outfile)
+            if _size <= target_size:
+                print "succeed in", args, 'size:', _size
+                break
 
 
 def shunk_img(im, infile, out, bound, **kwargs):
     print "shunk_img:", infile
     if im.format.lower() == "gif":
-        shunk_gif(infile, out)
+        shunk_gif(infile, out, target_size=None, **kwargs)
     else:
         factor = min(float(bound[0]) / im.size[0],
                      float(bound[1]) / im.size[1])
