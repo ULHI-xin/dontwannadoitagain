c515dd76d2a8a35a46562656e955b93dc1168baf
ULHI-Xin <xin@upwlabs.com>
2019-10-27 15:33:12 +0800

    check-in pack zip tool.

---
+++ eh-scrawler/pack.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import os
+import sys
+import subprocess
+from datetime import datetime
+
+
+root = sys.argv[1]
+for r, d, f in os.walk(root):
+    datelabel = datetime.now().strftime('%Y%m%d')
+    for _d in d:
+        if not _d or _d == 'bak':
+            continue
+        zipname = _d.decode("utf-8")
+        zipname = zipname.replace(" ", "_")
+        if not _d.startswith(datelabel):
+            zipname = datelabel + "_" + zipname
+        zipname = zipname + ".zip"
+
+        args = ["zip", "-r", root + "/" + zipname, root + "/" + _d]
+        print(args)
+        subprocess.call(args)
