a67972cb8c82419f533b316f3f4f5e143e455f2e
ULHI-Xin <xin@upwlabs.com>
2020-03-01 12:45:42 +0800

    remove datelabel.

--- eh-scrawler/pack.py
+++ eh-scrawler/pack.py
@@ -9,14 +9,11 @@ from datetime import datetime
 
 root = sys.argv[1]
 for r, d, f in os.walk(root):
-    datelabel = datetime.now().strftime('%Y%m%d')
     for _d in d:
         if not _d or _d == 'bak':
             continue
         zipname = _d.decode("utf-8")
         zipname = zipname.replace(" ", "_").replace("/", "-")
-        if not _d.startswith(datelabel):
-            zipname = datelabel + "_" + zipname
         zipname = zipname + ".zip"
 
         args = ["zip", "-r", root + "/" + zipname, root + "/" + _d]
