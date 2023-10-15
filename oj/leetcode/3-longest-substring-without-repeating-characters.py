47ba45ae3a9b41df9cb1337c46c2c387f6e34a59
Xin Zhao <xin@glowing.com>
2023-10-15 12:57:27 +0800

    check-in a new approach for lc-3.

--- oj/leetcode/3-longest-substring-without-repeating-characters.py
+++ oj/leetcode/3-longest-substring-without-repeating-characters.py
@@ -16,3 +16,27 @@ class Solution:
                 rightmost = rightmost[rightmost.find(c) + 1:]
                 rightmost_len = len(rightmost)
         return max(rightmost_len, non_rightmost_len)
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substr_idx = {}
+        max_sub_len = 0
+        substr_l = 0
+        for idx, c in enumerate(s):
+            if c not in substr_idx:
+                substr_idx[c] = idx
+                max_sub_len = max(max_sub_len, len(substr_idx))
+                continue
+            idx_to_trunc = substr_idx[c]
+            for l in range(substr_l, idx_to_trunc + 1):
+                substr_idx.pop(s[l])
+            substr_l = idx_to_trunc + 1
+            substr_idx[c] = idx
+            max_sub_len = max(max_sub_len, len(substr_idx))
+        return max_sub_len
+
+
+s = "abcabcbb"
+s = "bbbbb"
+s = "pwwkew"
+print(Solution().lengthOfLongestSubstring(s))
